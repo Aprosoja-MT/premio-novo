77c7ef27381497344f2d35cc009274bf24ea72ab
iang-ts <ian.dev.empresarial@gmail.com>
2026-03-16 15:02:50 -0400

    clear project

--- app/routes/$.tsx
+++ app/routes/$.tsx
@@ -1,7 +1,6 @@
 import type { Route } from "./+types/$";
 
 export async function loader({ request }: Route.LoaderArgs) {
-  // Ignora requisições do Chrome DevTools
   const url = new URL(request.url);
   if (url.pathname.startsWith("/.well-known/")) {
     return new Response(null, { status: 404 });
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,20 +1,5 @@
-import { redirect } from "react-router";
-import { Landing } from "ui/pages/Landing";
-import type { Route } from "./+types/home";
+import { HomePage } from "@/pages/home";
 
-export type LandingProps = Pick<Route.ComponentProps, "loaderData" | "actionData">;
-
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
-}
-
-export async function action({}: Route.ActionArgs) {
-  return redirect('/subscription')
-}
-
-export default function Home({ loaderData }: Route.ComponentProps) {
-  return <Landing loaderData={loaderData} />;
+export default function HomeRoute() {
+  return <HomePage />;
 }
