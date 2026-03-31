9eb26bda701a3bb16b4906a17a679214ecf0a065
iang-ts <ian.dev.empresarial@gmail.com>
2026-03-31 09:17:55 -0400

    update texts

--- ui/pages/home/components/CategoriesSection/useCategoriesSectionController.ts
+++ ui/pages/home/components/CategoriesSection/useCategoriesSectionController.ts
@@ -4,7 +4,7 @@ import type { SwiperRef } from 'swiper/react';
 export const CATEGORY_CARDS = [
   {
     title: 'Reportagem\nem Vídeo',
-    text: 'Matéria jornalística veiculada em canais de televisão (rede aberta, por assinatura ou streaming) ou em canais da plataforma Youtube ou de "Podcasts" na Internet, em língua portuguesa, produzidos e publicados no Estado de Mato Grosso, com pertinência temática ao concurso, e com duração igual ou menor seis minutos, fora o tempo de cabeça – chamada do apresentador.',
+    text: 'Matéria jornalística veiculada em canais de televisão (rede aberta, por assinatura ou streaming) ou em canais da plataforma Youtube ou de "Podcasts" na Internet, em língua portuguesa, produzidos e publicados no Brasil, com pertinência temática ao concurso, e com duração igual ou inferior a 10 (dez) minutos, não computado o tempo de cabeça, introdução ou chamada do apresentador.',
   },
   {
     title: 'Reportagem\nem Texto',
@@ -12,19 +12,19 @@ export const CATEGORY_CARDS = [
   },
   {
     title: 'Reportagem\nem Áudio',
-    text: 'Matéria jornalística veiculada em emissoras de rádio ou em canais ou plataformas de "Streaming" ou "Podcasts" na Internet, em língua portuguesa, produzidos e publicados no Estado de Mato Grosso, com pertinência temática ao concurso, e com duração igual ou menor seis minutos, fora o tempo de cabeça – chamada do apresentador.',
+    text: 'Matéria jornalística veiculada em emissoras de rádio ou em canais ou plataformas de "Streaming" ou "Podcasts" na Internet, em língua portuguesa, produzidos e publicados no Estado de Mato Grosso, com pertinência temática ao concurso, com duração igual ou inferior a 10 (dez) minutos, não computado o tempo de cabeça, introdução ou chamada do apresentador.',
   },
   {
     title: 'Foto-\njornalismo',
-    text: 'Fotografia jornalística publicada em veículos impressos, digitais ou redes sociais de caráter jornalístico, em língua portuguesa, produzida e publicada no Estado de Mato Grosso, com pertinência temática ao concurso.',
+    text: 'Foto ou sequência de fotos publicadas em matéria de veículos de imprensa ou de comunicação (jornais, revistas, portais de notícias ou de conteúdo na internet), sediados no Brasil, com pertinência temática ao concurso, e que não possuam caráter meramente artístico ou ainda efeitos especiais de edição e nem sejam produtos de ensaios de imagens ou de realização por cinegrafistas ou fotógrafos amadores (que não tenha registro profissional).',
   },
   {
     title: 'Jornalismo\nUniversitário',
-    text: 'Produção jornalística realizada por estudantes de graduação em Jornalismo ou Comunicação Social de instituições de ensino superior do Estado de Mato Grosso, com pertinência temática ao concurso.',
+    text: 'Conteúdo jornalístico (nas modalidades Texto, Áudio, Vídeo ou Foto), produzidos e publicados por alunos regularmente matriculados em curso de graduação em Comunicação Social, com habilitação em Jornalismo, de Instituições de Ensino Superior públicas ou privadas sediadas no Brasil, com pertinência temática ao concurso e observados parâmetros de forma e duração tratados nas categorias anteriores.',
   },
   {
     title: 'Destaques\nMato-grossenses',
-    text: 'Categoria exclusiva para jornalistas que atuam no Estado de Mato Grosso, reconhecendo profissionais com trajetória de destaque na cobertura do agronegócio e da cadeia produtiva da soja e milho.',
+    text: 'Matéria jornalística em vídeo, texto, áudio ou foto, ainda que por autor sem registro profissional em jornalismo, publicada em veículos de imprensa e de comunicação já descritos nas categorias anteriores, produzidos e veiculados em âmbito regional estadual, ou seja, nas regiões Norte, Sul, Leste, Oeste e da Baixada Cuiabana do Estado de Mato Grosso; e com pertinência temática ao concurso.',
   },
 ];
 
